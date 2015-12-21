90709f59c22987227992768ffb0c0f26ac6fb173
vgil <victor.gil.sepulveda@gmail.com>
2015-12-21 23:06:22 +0100

    added script for domain distances

--- nma_algo_char/acceptance_analyses.py
+++ nma_algo_char/acceptance_analyses.py
@@ -15,12 +15,19 @@ import numpy
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.colors import ListedColormap
+import cPickle as pickle
+from anmichelpers.tools.measure import coords_rmsf
+from trajectory_comparison.compare_two_rmsfs import rms
 
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("--experiment", dest="experiment")
     parser.add_option("--results", dest="results_folder")
     parser.add_option("--workspace", dest="workspace")
+    parser.add_option("-d", dest="data")
+    parser.add_option("--rmsf", dest="rmsf")
+    
+    
     
     (options, args) = parser.parse_args()
     
@@ -37,52 +44,90 @@ if __name__ == '__main__':
         workspace = os.path.normpath(experiment_details["workspace"])
     
     # create a place for the results
-    create_directory(os.path.join(options.results_folder, os.path.basename(workspace)))
+    results_folder = os.path.join(options.results_folder, os.path.basename(workspace))
+    create_directory(results_folder)
 
     # Things to load
     acceptances = defaultdict(lambda: defaultdict(list))
-    avg_energy = defaultdict(list)
-    std_energy = defaultdict(list)
-    avg_rmsd = defaultdict(list)
-    std_rmsd = defaultdict(list)
+    def dd_float():
+        return defaultdict(float)
+    avg_energy = defaultdict(dd_float)
+    std_energy = defaultdict(dd_float)
+    avg_rmsd = defaultdict(dd_float)
+    std_rmsd = defaultdict(dd_float)
+    rmsf = defaultdict(dict)
     p1_keys = []
     p2_keys = []
-    acceptance_temperatures = [300, 500, 700, 900, 1000, 1200, 1500, 1700, 1900, 2100, 2300, 2500, 2700, 2900]
+    acceptance_temperatures = [300, 866, 1432, 2000, 2568, 3000]
     
-    for (p1,v1),(p2,v2) in pair_parameter_values(experiment_details["check"], experiment_details["parameter_values"]):
-        folder_name = "%s_%s_%s_%s_%s"%(experiment_details["prefix"],
-                                            experiment_details["parameter_abbv"][p1], parameter_value_to_string(v1),
-                                            experiment_details["parameter_abbv"][p2], parameter_value_to_string(v2))
-        
-        
-        ## CAUTION: HARDCODED FOLDER ('info'). Must be extracted using the control file
-        if experiment_details["prefix"] == "CC":
-            raw_data, min_len = load_data(os.path.join(workspace, folder_name,"info"), 
-                                          "perturb_energy_before.log",
-#                                           "perturb_energy_after.log", 
-                                        "final_energy.log",   # Energy of the whole step
-                                          "initial_cc.log", 
-                                          "after_anm_cc.log", 
-                                          "step_time.log")
-        
-        if experiment_details["prefix"] == "IC":
-            raw_data, min_len = load_ic_data(os.path.join(workspace, folder_name,"info"))
-        
-        # skip first frame (usually an outlayer)
-        #print "DBG", min_len-1, len(modes), len(process_modes(experiment_details["prefix"], modes, 10))
-        energy_increments = process_energy_differences(raw_data)[1:]
-        rmsd_increments = process_after_perturb_rmsd(raw_data)[1:]
-        
-        mc = MetropolisMCSimulator(energy_increments)
-        for T in acceptance_temperatures :
-            acceptances[T][v1,v2] = mc.perform_simulation(min(50,len(energy_increments)), 40, T)
-            print acceptances[T][v1,v2]
-        avg_energy[v1,v2] = numpy.mean(energy_increments)
-        std_energy[v1,v2] = numpy.std(energy_increments)
-        avg_rmsd[v1,v2] = numpy.mean(rmsd_increments)
-        std_rmsd[v1,v2] = numpy.std(rmsd_increments)
-        p1_keys.append(v1)
-        p2_keys.append(v2)
+    if options.data is None:
+        for T in acceptance_temperatures:
+            for (p1,v1),(p2,v2) in pair_parameter_values(experiment_details["check"], experiment_details["parameter_values"]):
+                
+                folder_name = "%s_%s_%s_%s_%s"%(experiment_details["prefix"],
+                                                experiment_details["parameter_abbv"][p1], parameter_value_to_string(v1),
+                                                experiment_details["parameter_abbv"][p2], parameter_value_to_string(v2))
+                
+                main_folder = "%s_%d"%(experiment_details["prefix"],T)
+                data_folder = os.path.join(main_folder, workspace, folder_name,"info")
+                
+                print data_folder
+                
+                ## CAUTION: HARDCODED FOLDER ('info'). Must be extracted using the control file
+                if experiment_details["prefix"] == "CC":
+                    raw_data, min_len = load_data(data_folder, 
+                                                  "perturb_energy_before.log",
+        #                                           "perturb_energy_after.log", 
+                                                "final_energy.log",   # Energy of the whole step
+                                                  "initial_cc.log", 
+#                                                   "after_anm_cc.log",
+                                                  "after_minimization_cc.log", 
+                                                  "step_time.log")
+                
+                if experiment_details["prefix"] == "IC":
+                    raw_data, min_len = load_ic_data(os.path.join(workspace, folder_name,"info"))
+                
+                # skip first frame (usually an outlayer)
+                #print "DBG", min_len-1, len(modes), len(process_modes(experiment_details["prefix"], modes, 10))
+                energy_increments = process_energy_differences(raw_data)[1:]
+                rmsd_increments = process_after_perturb_rmsd(raw_data)[1:]
+                
+                mc = MetropolisMCSimulator(energy_increments)
+                acceptances[T][v1,v2] = mc.perform_simulation(min(200,len(energy_increments)), 20, T)
+                avg_energy[T][v1,v2] = numpy.mean(energy_increments)
+                std_energy[T][v1,v2] = numpy.std(energy_increments)
+                avg_rmsd[T][v1,v2] = numpy.mean(rmsd_increments)
+                std_rmsd[T][v1,v2] = numpy.std(rmsd_increments)
+                p1_keys.append(v1)
+                p2_keys.append(v2)
+                
+                # Rmsf calculations
+                rmsf_coords = numpy.reshape(raw_data["coords_after"], (len(raw_data["coords_after"]), 
+                                                                           len(raw_data["coords_after"][0])/3, 
+                                                                           3))
+                rmsf[T][v1,v2] = coords_rmsf(rmsf_coords)
+                
+        def default_to_regular(d):
+            if isinstance(d, defaultdict):
+                d = {k: default_to_regular(v) for k, v in d.iteritems()}
+            return d
+        pickle.dump((default_to_regular(acceptances), 
+                     default_to_regular(avg_energy), 
+                     default_to_regular(std_energy), 
+                     default_to_regular(avg_rmsd), 
+                     default_to_regular(std_rmsd),
+                     rmsf, 
+                     p1, p1_keys, 
+                     p2, p2_keys),
+                    open(os.path.join(results_folder,"data"),"w"))
+    else:
+        (acceptances, 
+         avg_energy, std_energy, 
+         avg_rmsd, std_rmsd, 
+         rmsf,
+         p1, p1_keys, 
+         p2, p2_keys) = pickle.load(open(options.data))
+    
     
     p1_keys = sorted(list(set(p1_keys)))
     p2_keys = sorted(list(set(p2_keys)))   
@@ -103,10 +148,14 @@ if __name__ == '__main__':
         for v1 in p1_keys:
             for v2 in p2_keys:
                 if cat_acceptances[T][v1,v2] == "good":
-                    good_parameters[T].append((avg_rmsd[v1,v2]-std_rmsd[v1,v2], avg_rmsd[v1,v2]+std_rmsd[v1,v2],avg_rmsd[v1,v2], p1, v1, p2, v2))
+                    good_parameters[T].append((avg_rmsd[T][v1,v2]-std_rmsd[T][v1,v2], 
+                                               avg_rmsd[T][v1,v2]+std_rmsd[T][v1,v2],
+                                               avg_rmsd[T][v1,v2], 
+                                               p1, v1, 
+                                               p2, v2))
     
-    row_len = 4
-    col_len = 4
+    row_len = 3
+    col_len = 2
     f, axes = plt.subplots( col_len, row_len, sharey='row', sharex='col')
     f.subplots_adjust(hspace=0.4, wspace=0.3 )
     f.set_size_inches(10, 12, forward=True)
@@ -114,14 +163,17 @@ if __name__ == '__main__':
     for i,T in enumerate(acceptance_temperatures):
         for j,v1 in enumerate(p1_keys):
             for k,v2 in enumerate(p2_keys):
-                matrix[j][k] = cat_to_float[cat_acceptances[T][v1,v2]]
+                matrix[j][k] = acceptances[T][v1,v2][0]#cat_to_float[cat_acceptances[T][v1,v2]]
         print i, i/row_len, i%row_len
         ax = axes[i/row_len, i%row_len] 
-        sns.heatmap(matrix, cmap=ListedColormap(['red', 'green', 'yellow']), ax = ax, square = False, cbar = False, center = 0.5)
+        sns.heatmap(matrix, #cmap=ListedColormap(['red', 'green', 'yellow']), 
+                    ax = ax, square = False, cbar = False, center = 0.5,
+                    annot = True)
         ax.set_xticklabels([str(i) for i in p2_keys])
         ax.set_yticklabels([str(i) for i in sorted(p1_keys, reverse=True)])
         ax.set_title("T = %d"%T)
     plt.show()
+    plt.close()
     
     # for each displacement pick smaller temperature
     for T in sorted(good_parameters.keys()):
@@ -129,3 +181,25 @@ if __name__ == '__main__':
         print T, 
         for x in good_parameters[T][0]: print x,
         print
+
+    # Do rmsf comparisons
+    if options.rmsf:
+        f, axes = plt.subplots( col_len, row_len, sharey='row', sharex='col')
+        f.subplots_adjust(hspace=0.4, wspace=0.3 )
+        f.set_size_inches(10, 12, forward=True)
+        rmsf_matrix = numpy.zeros((len(p1_keys), len(p2_keys)))
+        rmsf_ref = numpy.loadtxt(options.rmsf)[:-1]
+        for i,T in enumerate(acceptance_temperatures):
+            for j,v1 in enumerate(p1_keys):
+                for k,v2 in enumerate(p2_keys):
+                    rmsf_matrix[j][k] = rms(rmsf_ref,rmsf[T][v1,v2])
+#                     plt.plot(rmsf_ref)
+#                     plt.plot(rmsf[T][v1,v2])
+#                     plt.show()
+#                     plt.close()
+            ax = axes[i/row_len, i%row_len] 
+            sns.heatmap(rmsf_matrix, #cmap=ListedColormap(['red', 'green', 'yellow']), 
+                    ax = ax, square = False, cbar = False, #center = 0.5,
+                    annot = True)
+        plt.show()
+        plt.close()
